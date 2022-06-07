cbe79b69ec10dfe4ecc33090f266de32556d2950
WordChained <talbd51@gmail.com>
2022-06-07 09:40:52 +0300

    .

--- src/components/shared/headers/LeftHeader.jsx
+++ src/components/shared/headers/LeftHeader.jsx
@@ -1,13 +1,14 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { UsersContext } from '../../../store/contexts/UsersContext';
-import defaultProfileIcon from '../../../assets/icons/user.png';
-import statusIcon from '../../../assets/imgs/charging-circle.png';
-import messageIcon from '../../../assets/imgs/comment.png';
-import moreIcon from '../../../assets/imgs/more.png';
-import styles from './LeftHeader.module.css';
+import React, { useContext, useEffect, useState } from "react";
+import { UsersContext } from "../../../store/contexts/UsersContext";
+import defaultProfileIcon from "../../../assets/icons/user.png";
+import statusIcon from "../../../assets/imgs/charging-circle.png";
+import messageIcon from "../../../assets/imgs/comment.png";
+import moreIcon from "../../../assets/imgs/more.png";
+import styles from "./LeftHeader.module.css";
 export const LeftHeader = () => {
   const { usersState } = useContext(UsersContext);
   const [profileImage, setProfileImage] = useState(defaultProfileIcon);
+
   const isImageExists = async () => {
     const img = await usersState.loggedInUser.profileImage;
     setProfileImage(img ? img : defaultProfileIcon);
@@ -18,12 +19,12 @@ export const LeftHeader = () => {
   return (
     <header className={styles.leftHeaderContainer}>
       <div className={styles.profileImageContainer}>
-        <img src={profileImage} alt='' />
+        <img src={profileImage} alt="" />
       </div>
       <div className={styles.rightSideIcons}>
-        <img src={statusIcon} alt='' />
-        <img src={messageIcon} alt='' />
-        <img src={moreIcon} alt='' />
+        <img src={statusIcon} alt="" />
+        <img src={messageIcon} alt="" />
+        <img src={moreIcon} alt="" />
       </div>
     </header>
   );
